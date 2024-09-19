d9ffac5a091063d9a96ee6dc515e4eeccd4f1320
victorhabila <48987601+victorhabila@users.noreply.github.com>
2024-09-19 16:23:20 +0200

    index page update

--- ecommerce-app/api/index.js
+++ ecommerce-app/api/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 const jwt = require("jsonwebtoken");
 mongoose
   .connect(
-    "mongodb+srv://victorhabila:victorhabila1994.com@cluster0.ck26w.mongodb.net/",
+    "mongodb+srv://victorhabila:{your pass}@cluster0.ck26w.mongodb.net/",
     {
       //useNewUrlParser: true,
       //useUnifiedTopology: true,
